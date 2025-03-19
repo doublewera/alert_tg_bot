13e18d822759ef56546defcf90cf84163df3fea4
Wera Barinova <alisawera@yandex.ru>
2025-03-19 18:35:34 +0300

    Теперь у бота новая забота: посылать сообщения
    Создана группа асинхронных задач
    Получение сообщений от пользователей
    и запись в канал происходят независимо

--- notify_event.py
+++ notify_event.py
@@ -4,10 +4,19 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 
-from secret import TOKEN
+from datetime import datetime
+
+from secret import TOKEN, space_events_id
 
 dp = Dispatcher()
+bot = Bot(token=TOKEN)
 
+async def check_db() -> None:
+    while True:
+        await asyncio.sleep(10)
+        await bot.send_message(
+            space_events_id,
+            "Я проснулся! %s" % datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
 # Command handler
 @dp.message(Command("start"))
@@ -16,8 +25,12 @@ async def command_start_handler(message: Message) -> None:
 
 # Run the bot
 async def main() -> None:
-    bot = Bot(token=TOKEN)
-    await dp.start_polling(bot)
+    tasks = []
+    async with asyncio.TaskGroup() as tg:
+        tasks.append(
+            tg.create_task(check_db()))
+        tasks.append(
+            tg.create_task(dp.start_polling(bot)))
 
 
 if __name__ == "__main__":
